f771120ba352e35f0b086ea76b86de89015a89d6
Zhentao <shizhentao@gmail.com>
2021-04-26 23:11:41 +0800

    add the package information script (firt draft)

---
+++ R_pkg_fsPDA/R/fsPDA.R
@@ -0,0 +1,43 @@
+#' fsPDA: Package for the forward-selected panel data approach
+#'
+#' Estimation and inference by the forward-selected panel data approach in Shi and Huang (2021)
+#'
+#' \code{est.fsPDA} is the main function. It generates a \code{fsPDA} object which can work with
+#' the generic methods \code{plot}.
+#'
+#' @references
+#' Zhentao Shi and Jingyi Huang (2021): "Forward-Selected Panel Data Approach for Program Evaluation,"
+#'   forthcoming at the Journal of Econometrics, arXiv: 1908.05894
+#'
+#' @docType package
+#' @name bHP
+NULL
+
+
+
+#' Chinese import data
+#'
+#' @usage data(china_import)
+#'
+
+#' @format
+#' \itemize{
+#'  \item\strong{Release:} {United Nations}
+#'  \item\strong{Frequency:} {Annual}
+#'  \item\strong{Date Range:} {20xx -- 20xx}
+#' }
+#'
+#' @section Descripton:
+#' This dataset is described in Section 5 of Shi and Huang (2021).
+#'
+#'
+#' @source United Nations Comtrade Database \url{https://comtrade.un.org/}
+#'
+#' @references
+#'
+#' Zhentao Shi and Jingyi Huang (2021): "Forward-Selected Panel Data Approach for Program Evaluation,"
+#'   forthcoming at the Journal of Econometrics, arXiv: 1908.05894
+#'
+#'
+#' @docType data
+"china_import"
