8e2cf54d47ce00c2c9133360c1f2a37602ff9d6a
Zhentao <shizhentao@gmail.com>
2021-04-26 23:33:59 +0800

    a working version after adding the package information in 'fsPDA.R'

--- R_pkg_fsPDA/R/fsPDA.R
+++ R_pkg_fsPDA/R/fsPDA.R
@@ -10,34 +10,6 @@
 #'   forthcoming at the Journal of Econometrics, arXiv: 1908.05894
 #'
 #' @docType package
-#' @name bHP
+#' @name fsPDA
 NULL
 
-
-
-#' Chinese import data
-#'
-#' @usage data(china_import)
-#'
-
-#' @format
-#' \itemize{
-#'  \item\strong{Release:} {United Nations}
-#'  \item\strong{Frequency:} {Annual}
-#'  \item\strong{Date Range:} {20xx -- 20xx}
-#' }
-#'
-#' @section Descripton:
-#' This dataset is described in Section 5 of Shi and Huang (2021).
-#'
-#'
-#' @source United Nations Comtrade Database \url{https://comtrade.un.org/}
-#'
-#' @references
-#'
-#' Zhentao Shi and Jingyi Huang (2021): "Forward-Selected Panel Data Approach for Program Evaluation,"
-#'   forthcoming at the Journal of Econometrics, arXiv: 1908.05894
-#'
-#'
-#' @docType data
-"china_import"
