4b7367a07a9d86f36e6a516c460fc47abc44bd5e
Zhentao <shizhentao@gmail.com>
2021-04-26 22:36:43 +0800

    format the style

--- R_pkg_fsPDA/R/fsPDA.R
+++ R_pkg_fsPDA/R/fsPDA.R
@@ -29,98 +29,104 @@
 #' library(fsPDA)
 #' data("testData")
 #'
-#' treated = testData$treated
-#' control = testData$control
-#' intervention_time = testData$intervention_time
+#' treated <- testData$treated
+#' control <- testData$control
+#' intervention_time <- testData$intervention_time
 #'
-#' result=fsPDA(treated, control,
-#'             treatment_start = which(names(treated) == intervention_time),
-#'             date = as.Date(paste(substr(names(treated),1,4), "-", substr(names(treated), 5, 6), "-01", sep="")))
+#' result <- fsPDA(treated, control,
+#'   treatment_start = which(names(treated) == intervention_time),
+#'   date = as.Date(paste(substr(names(treated), 1, 4), "-", substr(names(treated), 5, 6), "-01", sep = ""))
+#' )
 #'
 #' print(result)
-#'
 #' @export
 #'
 #'
 #' @references
-#' Zhentao Shi and Jingyi Huang (2001): "Forward-Selected Panel Data Approach for Program Evaluation,"
-#'   Journal of Econometrics, arXiv: 1908.05894
+#' Zhentao Shi and Jingyi Huang (2021): "Forward-Selected Panel Data Approach for Program Evaluation,"
+#'   forthcoming at the Journal of Econometrics, arXiv: 1908.05894
 #'
 
 
-fsPDA=function(treated,control,treatment_start,date=NULL,lrvar_lag=NULL) {
+fsPDA <- function(treated, control, treatment_start, date = NULL, lrvar_lag = NULL) {
 
 
   # check inputs
 
-  if(is.matrix(control)) {
-    N=ncol(control)
+  if (is.matrix(control)) {
+    N <- ncol(control)
   } else {
-    stop("control should be a matrix")
+    stop("control must be a matrix")
   }
 
-  if(length(treated)==nrow(control)) {
-    Tn=length(treated)
+  if (length(treated) == nrow(control)) {
+    Tn <- length(treated)
   } else {
-    stop("lengths of treated and control units should be the same")
+    stop("lengths of treated and control units must be the same")
   }
 
-  if(treatment_start==floor(treatment_start) & treatment_start>0.5*Tn & treatment_start<=Tn) {
-    T1=treatment_start-1
-    T2=Tn-T1
+  if (treatment_start == floor(treatment_start) & treatment_start > 0.5 * Tn & treatment_start <= Tn) {
+    T1 <- treatment_start - 1
+    T2 <- Tn - T1
   } else {
-    stop("treatment_start should be an integer no larger than the total sample size with enough pre-treatment periods")
+    stop("treatment_start must be an integer no larger than the total sample size with enough pre-treatment periods")
   }
 
-  if(is.null(date)) {
-    date=1:Tn
+  if (is.null(date)) {
+    date <- 1:Tn
   } else {
-    if (length(date)!=Tn) {
-      stop("date should be of the same length with the total sample size")
+    if (length(date) != Tn) {
+      stop("date must be of the same length with the total sample size")
     }
   }
 
-  if(is.null(lrvar_lag)) {
-    lrvar_lag=floor(T2^(1/4))
+  if (is.null(lrvar_lag)) {
+    lrvar_lag <- floor(T2^(1 / 4))
   } else {
-    if(lrvar_lag!=floor(lrvar_lag) | lrvar_lag<0 | lrvar_lag>sqrt(T2)) {
-      stop("lrvar_lag should be a non-negative integer no larger than sqrt of post-treated sample size")
+    if (lrvar_lag != floor(lrvar_lag) | lrvar_lag < 0 | lrvar_lag > sqrt(T2)) {
+      stop("lrvar_lag must be a non-negative integer no larger than sqrt of post-treated sample size")
     }
   }
 
 
   # sample splitting
 
-  y1=treated[1:T1];y2=treated[(T1+1):Tn]
-  Y1=control[1:T1,,drop=FALSE];Y2=control[(T1+1):Tn,,drop=FALSE]
+  y1 <- treated[1:T1]
+  y2 <- treated[(T1 + 1):Tn]
+  Y1 <- control[1:T1, , drop = FALSE]
+  Y2 <- control[(T1 + 1):Tn, , drop = FALSE]
 
 
   # forward iteration
 
-  var.resid=function(j,Y_select) {
-    X=cbind(1,Y_select,Y1[,j])
-    XX=t(X)%*%X
-    XX_inv=try(solve(XX),silent=TRUE)
-    if ("try-error" %in% class(XX_inv)) {XX_inv=MASS::ginv(XX)}
-    b_hat=XX_inv%*%t(X)%*%y1
-    e_hat=y1-as.vector(X%*%b_hat)
-    sigma2_hat=mean(e_hat^2)
+  var.resid <- function(j, Y_select) {
+    X <- cbind(1, Y_select, Y1[, j])
+    XX <- t(X) %*% X
+    XX_inv <- try(solve(XX), silent = TRUE)
+    if ("try-error" %in% class(XX_inv)) {
+      XX_inv <- MASS::ginv(XX)
+    }
+    b_hat <- XX_inv %*% t(X) %*% y1
+    e_hat <- y1 - as.vector(X %*% b_hat)
+    sigma2_hat <- mean(e_hat^2)
     return(sigma2_hat)
   }
 
-  B=log(log(N))*log(T1)/T1
-  IC=log(var(y1))
-  select=NULL;R=0
-  left=1:N
+  B <- log(log(N)) * log(T1) / T1
+  IC <- log(var(y1))
+  select <- NULL
+  R <- 0
+  left <- 1:N
   for (r in 1:T1) {
-    sigma2_grid=sapply(left,FUN=var.resid,Y_select=Y1[,select])
-    sigma2_min=min(sigma2_grid)
-    select_1=left[which(sigma2_grid==sigma2_min)]
-    IC_1=log(sigma2_min)+B*(R+length(select_1))
-    if(IC_1<IC) {
-      IC=IC_1
-      select=c(select,select_1);R=length(select)
-      left=setdiff(left,select_1)
+    sigma2_grid <- sapply(left, FUN = var.resid, Y_select = Y1[, select])
+    sigma2_min <- min(sigma2_grid)
+    select_1 <- left[which(sigma2_grid == sigma2_min)]
+    IC_1 <- log(sigma2_min) + B * (R + length(select_1))
+    if (IC_1 < IC) {
+      IC <- IC_1
+      select <- c(select, select_1)
+      R <- length(select)
+      left <- setdiff(left, select_1)
     } else {
       break
     }
@@ -129,54 +135,57 @@ fsPDA=function(treated,control,treatment_start,date=NULL,lrvar_lag=NULL) {
 
   # post-selection estimation
 
-  X=cbind(1,Y1[,select,drop=FALSE])
-  XX=t(X)%*%X
-  XX_inv=try(solve(XX),silent=TRUE)
-  if ("try-error" %in% class(XX_inv)) {XX_inv=MASS::ginv(XX)}
-  b_hat=as.vector(XX_inv%*%t(X)%*%y1)
+  X <- cbind(1, Y1[, select, drop = FALSE])
+  XX <- t(X) %*% X
+  XX_inv <- try(solve(XX), silent = TRUE)
+  if ("try-error" %in% class(XX_inv)) {
+    XX_inv <- MASS::ginv(XX)
+  }
+  b_hat <- as.vector(XX_inv %*% t(X) %*% y1)
 
-  beta_hat=rep(0,N);beta_hat[select]=b_hat[-1]
-  beta_hat=c(b_hat[1],beta_hat)
-  y1_hat=as.vector(X%*%b_hat)
-  RSquared=var(y1_hat)/var(y1)
+  beta_hat <- rep(0, N)
+  beta_hat[select] <- b_hat[-1]
+  beta_hat <- c(b_hat[1], beta_hat)
+  y1_hat <- as.vector(X %*% b_hat)
+  RSquared <- var(y1_hat) / var(y1)
 
 
   # counterfactual and treatment effect
 
-  y2_0=as.vector(cbind(1,Y2[,select,drop=FALSE])%*%b_hat)
-  d_hat=y2-y2_0
-  ATE=mean(d_hat)
+  y2_0 <- as.vector(cbind(1, Y2[, select, drop = FALSE]) %*% b_hat)
+  d_hat <- y2 - y2_0
+  ATE <- mean(d_hat)
 
 
   # test of ATE with Newey-West lrvar (Bartlett kernel)
 
-  if(lrvar_lag>0) {
-    gamma_d=as.vector(acf(d_hat,lag.max=lrvar_lag,type="covariance",plot=FALSE)$acf)
-    w=1-(1:lrvar_lag)/(lrvar_lag+1)
-    lrvar_d=gamma_d[1]+2*sum(w*gamma_d[-1])
+  if (lrvar_lag > 0) {
+    gamma_d <- as.vector(acf(d_hat, lag.max = lrvar_lag, type = "covariance", plot = FALSE)$acf)
+    w <- 1 - (1:lrvar_lag) / (lrvar_lag + 1)
+    lrvar_d <- gamma_d[1] + 2 * sum(w * gamma_d[-1])
   } else {
-    lrvar_d=var(d_hat)
+    lrvar_d <- var(d_hat)
   }
 
-  Z=ATE/sqrt(lrvar_d/T2)
-  p_value=2*(1-pnorm(abs(Z)))
+  Z <- ATE / sqrt(lrvar_d / T2)
+  p_value <- 2 * (1 - pnorm(abs(Z)))
 
 
   # output
 
-  if(!is.null(colnames(control))) {
-    names(beta_hat)=c("intercept",colnames(control))
-    select=colnames(control)[select]
+  if (!is.null(colnames(control))) {
+    names(beta_hat) <- c("intercept", colnames(control))
+    select <- colnames(control)[select]
   }
 
-  result=list(select=list(dim=R,control=select,coef=beta_hat,RSquared=RSquared),
-              in_sample=data.frame(date=date[1:T1],observation=y1,fit=y1_hat),
-              out_of_sample=data.frame(date=date[(T1+1):Tn],observation=y2,counterfactual=y2_0,treatment=d_hat),
-              ATE=c(ATE=ATE,lrVar=lrvar_d,t_stat=Z,p_value=p_value))
-  class(result)="fsPDA"
+  result <- list(
+    select = list(dim = R, control = select, coef = beta_hat, RSquared = RSquared),
+    in_sample = data.frame(date = date[1:T1], observation = y1, fit = y1_hat),
+    out_of_sample = data.frame(date = date[(T1 + 1):Tn], observation = y2, counterfactual = y2_0, treatment = d_hat),
+    ATE = c(ATE = ATE, lrVar = lrvar_d, t_stat = Z, p_value = p_value)
+  )
+  class(result) <- "fsPDA"
 
 
   return(result)
 }
-
-
--- R_pkg_fsPDA/R/plot.fsPDA.R
+++ R_pkg_fsPDA/R/plot.fsPDA.R
@@ -1,6 +1,6 @@
 #' Plot method for objects of class `fsPDA`.
 #'
-#' @param x An object of class `fsPDA`.
+#' @param x An object of class the `fsPDA`.
 #' @param tlab The label of the time (horizontal) axis.
 #' @param ylab The label of the value (vertical) axis.
 #' @param title The text for the title.
@@ -12,46 +12,50 @@
 #'
 #' @examples
 #' library(fsPDA)
-#' library(ggplot2)
 #'
-#' data("testData")
+#' data("china_import")
 #'
-#' treated = testData$treated
-#' control = testData$control
-#' intervention_time = testData$intervention_time
+#' treated <- testData$treated
+#' control <- testData$control
+#' intervention_time <- testData$intervention_time
 #'
-#' result=fsPDA(treated, control,
-#'             treatment_start = which(names(treated) == intervention_time),
-#'             date = as.Date(paste(substr(names(treated),1,4), "-", substr(names(treated), 5, 6), "-01", sep="")))
+#' result <- fsPDA(treated, control,
+#'   treatment_start = which(names(treated) == intervention_time),
+#'   date = as.Date(paste(substr(names(treated), 1, 4), "-", substr(names(treated), 5, 6), "-01", sep = ""))
+#' )
 #'
 #' plot(result, tlab = "Year", ylab = "Monthly Growth Rate")
-#'
 #' @export
 #'
 #'
 
 
-plot.fsPDA=function(x,tlab=NULL,ylab=NULL,title=NULL,point=TRUE,legend.position="bottom") {
-
+plot.fsPDA <- function(x, tlab = NULL, ylab = NULL, title = NULL, point = TRUE, legend.position = "bottom") {
   suppressMessages(library(ggplot2))
 
-  ggdata=rbind(data.frame(date=x$in_sample$date,value=x$in_sample$observation,type="observation"),
-               data.frame(date=x$out_of_sample$date,value=x$out_of_sample$observation,type="observation"),
-               data.frame(date=c(x$in_sample$date,x$out_of_sample$date[1]),
-                          value=c(x$in_sample$fit,x$out_of_sample$counterfactual[1]),type="in-sample fit"),
-               data.frame(date=x$out_of_sample$date,value=x$out_of_sample$counterfactual,type="counterfactual"))
-  ggdata$type=factor(ggdata$type,levels=c("observation","in-sample fit","counterfactual"))
+  ggdata <- rbind(
+    data.frame(date = x$in_sample$date, value = x$in_sample$observation, type = "observation"),
+    data.frame(date = x$out_of_sample$date, value = x$out_of_sample$observation, type = "observation"),
+    data.frame(
+      date = c(x$in_sample$date, x$out_of_sample$date[1]),
+      value = c(x$in_sample$fit, x$out_of_sample$counterfactual[1]), type = "in-sample fit"
+    ),
+    data.frame(date = x$out_of_sample$date, value = x$out_of_sample$counterfactual, type = "counterfactual")
+  )
+  ggdata$type <- factor(ggdata$type, levels = c("observation", "in-sample fit", "counterfactual"))
 
-  plot=ggplot(ggdata,aes(x=date,y=value,color=type,linetype=type))+
-    geom_vline(xintercept=x$out_of_sample$date[1])+
-    geom_line()+scale_linetype_manual(values=c("solid","dashed", "dashed"))+
-    theme_bw()+theme(legend.title=element_blank(),legend.position=legend.position)+
-    labs(title=title) + xlab(tlab) + ylab(ylab)
-  if(point) {
-    plot=plot+geom_point()
+  plot <- ggplot(ggdata, aes(x = date, y = value, color = type, linetype = type)) +
+    geom_vline(xintercept = x$out_of_sample$date[1]) +
+    geom_line() +
+    scale_linetype_manual(values = c("solid", "dashed", "dashed")) +
+    theme_bw() +
+    theme(legend.title = element_blank(), legend.position = legend.position) +
+    labs(title = title) +
+    xlab(tlab) +
+    ylab(ylab)
+  if (point) {
+    plot <- plot + geom_point()
   }
 
   print(plot)
 }
-
-
