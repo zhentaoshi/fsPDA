c00effaaca9677e14bfaca5e0859f2330a20afa3
Zhentao <shizhentao@gmail.com>
2021-04-27 09:36:03 +0800

    add the documentation for the HCW data

--- R_pkg_fsPDA/R/fsPDA.R
+++ R_pkg_fsPDA/R/fsPDA.R
@@ -13,6 +13,10 @@
 #' @name fsPDA
 NULL
 
+
+
+
+
 #' Chinese import data
 #'
 #' @usage data(china_import)
@@ -38,3 +42,30 @@ NULL
 #'
 #' @docType data
 "china_import"
+
+
+
+
+
+#' HCW Data
+#'
+#' @usage data(HCW)
+#'
+#' This is the real GDP growth rate data used in Hsiao, Ching, and Wan (2012).
+#'
+
+#'
+#' @format
+#' \itemize{
+#'   \item\strong{panel} {A matrix of real GDP growth rate of many countries.}
+#'   \item\strong{quarter} {Character. A time sequence related to `panel` labeled by year and quarter.}
+#'   \item\strong{T1} {An Integer indicates the length of pre-treated period.}
+#'}
+#'
+#' @references
+#' Hsiao, C., S. H. Ching, and S. K. Wan (2012):
+#'   “A panel data approach for program evaluation: measuring the benefits of political and economic integration of Hong Kong with mainland China,”
+#'   Journal of Applied Econometrics, 27(5), 705–740.
+#'
+#' @docType data
+"HCW"
